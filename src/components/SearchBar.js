fdc9d7aaaa5e84cdb874583142e6cc25d46cadc7
Reem <janinareem@gmail.com>
2022-03-10 20:02:41 +0200

    Update searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,56 +1,15 @@
-import { VscSearch } from 'react-icons/vsc';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { displayCountries } from '../redux/actions/AllCountries';
 import '../styles/SearchBar.css';
 import img from '../images/map.jpg';
 
-const SearchBar = () => {
-  const [location, setLocation] = useState('');
-  const countries = useSelector((state) => state.CountriesReducer);
-  const dispatch = useDispatch();
-
-  const submitLocation = () => {
-    useEffect(() => {
-      if (countries.length === 0) {
-        dispatch(displayCountries());
-      }
-    }, []);
-  };
-
-  return (
-    <div className="all-cases">
-      <div className="first-section">
-        <img className="img-map" src={img} alt="map-world" />
-        <div className="info">
-          <h1 className="confimred-cases">All new Confrimed Cases</h1>
-        </div>
-      </div>
-      <div className="search">
-        <h2 className="h2Search">
-          Search by Country
-          <VscSearch className="search-icon" />
-        </h2>
-        <form
-          onSubmit={submitLocation()}
-          className="inputs"
-        >
-          <input
-            className="input-search"
-            placeholder="Search"
-            value={location}
-            onChange={(e) => { setLocation(e.target.value); }}
-          />
-          <button
-            className="btn"
-            type="button"
-          >
-            Submit
-          </button>
-        </form>
+const SearchBar = () => (
+  <div className="all-cases">
+    <div className="first-section">
+      <img className="img-map" src={img} alt="map-world" />
+      <div className="info">
+        <h1 className="confimred-cases">All new Confrimed Cases</h1>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default SearchBar;
